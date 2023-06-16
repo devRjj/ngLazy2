16576e1c92932da4aa658885aa0ef3160566ae26
Devraj Singh <devrajmac@Devrajs-MacBook-Air.local>
2023-06-16 19:10:32 +0530

    login_values_in_obj

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  loginForm!: FormGroup
+
+  constructor(private fb:FormBuilder){}
+
+  obj!: loginInterface[];
+
+  ngOnInit(){
+
+    this.createLogin()
+  }
+
+  createLogin(){
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    })
+  }
+
+  onSubmit(){
+    this.obj = this.loginForm.value;
+
+    console.log(this.obj);
+  }
+
+}
+
+export interface loginInterface{
+  username: string
+  password: string
 }
